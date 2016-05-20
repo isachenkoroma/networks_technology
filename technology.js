4081e1a6b2d68bbeecdd860683d05dc4d0b708c7
isachenkoroma <isa-ro@yandex.ru>
2016-05-20 23:58:19 +0400

    Add files via upload

---
+++ technology.js
@@ -0,0 +1,34 @@
+{
+	"technology": {
+		"2101": {
+			"id": 2101,
+			"picture": "shr/d/technology_picture/117262_3_3255_184516_004.png?ux=1442497198",
+			"title": "А� СЕНАЛ",
+			"info": "ИССЛЕДУЕТСЯ: Возможность построить Арсенал для производства боеприпасов.",
+			"order": 30,
+			"researchTime": 0.2,
+			"price": {
+				"253": "1",
+				"61": "20",
+				"65": "20"
+			},
+			"conditions": {
+				"conditions": {
+					"1": {
+						"tech": 2815,
+						"choice": "ConditionTech"
+					}
+				}
+			},
+			"discoveryList": {
+				"discoveryList": {
+					"1": {
+						"building": 513,
+						"choice": "DiscoveryBuilding"
+					}
+				}
+			},
+			"minVersion": null,
+			"maxVersion": null
+		}
+}
